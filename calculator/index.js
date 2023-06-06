3a3f2eef7e645d93c02b4d916dbdd3d49beca54c
AbdulWahabAwan <alvilahori@hotmail.com>
2023-06-06 23:02:55 +0500

    JavaScript Calculator

---
+++ calculator/index.js
@@ -0,0 +1,26 @@
+const buttonEl = document.querySelectorAll("button");
+const inputFieldEl = document.getElementById("result");
+
+for (let i = 0; i < buttonEl.length; i++) {
+  buttonEl[i].addEventListener("click", () => {
+    let buttonValue = buttonEl[i].textContent;
+    console.log(buttonValue);
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (buttonValue === "=") {
+      calculateResult();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+}
+function clearResult() {
+  inputFieldEl.value = "";
+}
+function calculateResult() {
+  inputFieldEl.value = eval(inputFieldEl.value);
+}
+function appendValue(buttonValue) {
+  inputFieldEl.value += buttonValue;
+  // inputFieldEl.value = inputFieldEl.value + buttonValue;
+}
